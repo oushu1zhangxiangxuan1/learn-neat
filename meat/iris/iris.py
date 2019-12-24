67f87478dc87b1ad217908613c44e17da0319625
oushu1zhangxiang1 <zhang.xiangxuan@oushu.io>
2019-12-24 21:33:33 +0800

    implement ZxxEncoder to encode string to onehot value

--- meat/iris/iris.py
+++ meat/iris/iris.py
@@ -4,6 +4,9 @@ import os
 import neat  
 import visualize 
 import pandas as pd 
+# import sklearn as 
+
+from sklearn.preprocessing import  OneHotEncoder
 
 
 iris_data = pd.read_csv("/Users/johnsaxon/test/github.com/learn-neat/meat/iris/testdata/iris_train.csv", header=None)
@@ -45,7 +48,23 @@ def eval_genomes(genomes, config):
             output = net.activate(row.values[:4])
             # 这里需要改用cross entropy when task is classification
             # genome.fitness -= (output[0]-row[4])**2
-            if output[0]!= wrow.values[4]:
+            if output[0]!= row.values[4]:
+                genome.fitness -= 1
+
+def eval_genomes(genomes, config, encoder):
+    for genome_id, genome in genomes:
+        genome.fitness = 4.0
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+        for i,row in iris_data.iterrows():
+            output = net.activate(row.values[:4])
+            # 这里需要改用cross entropy when task is classification
+            # genome.fitness -= (output[0]-row[4])**2
+
+            print(output)
+
+            encoder.transform(row.values[4])
+
+            if output[0]!= row.values[4]:
                 genome.fitness -= 1
 
 
@@ -86,6 +105,48 @@ def run(config_file):
     p.run(eval_genomes, 10)
 
 
+def test_run(config_file):
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                            neat.DefaultSpeciesSet, neat.DefaultStagnation, 
+                            config_file)
+
+    # create the population, which is the top-level object fir a neat run
+    p = neat.Population(config)
+
+    #Add a stdout reporter to show progress in the terninal
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    p.add_reporter(neat.Checkpointer(5))
+
+    # Run for up to 300 generations.
+    winner = p.run(eval_genomes, 1)
+
+    # #Display the winning genome.
+    # print('\n Best genome:\n{!s}'.format(winner))
+
+    # # Show output of the most fit genome against training data.
+    # print('\nOutput:')
+    # winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
+    # for i,row in iris_data.iterrows():
+    #     output = winner_net.activate(row.values[:4])
+    #     print('input {!r}, expected output {!r}, got {!r}'.format(
+    #         row.values[:4], row.values[4], output
+    #     ))
+
+    # # How does the node_names work?
+    # node_names = {-1:'A', -2:'B', -3:'C', -4:'D', 0:'label'}
+    # visualize.draw_net(config, winner, True, node_names=node_names)
+
+    # p = neat.Checkpointer.restore_checkpoint('neat-chepoint-4')
+    # p.run(eval_genomes, 10)
+
+
+def LabelEncoder(labels):
+    oh_enc = OneHotEncoder(n_values=labels)
+
+
+
 
 if __name__ == '__main__':
     # traverse_iris()
@@ -95,4 +156,6 @@ if __name__ == '__main__':
     # current working directory.
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, 'config_feedforward')
-    run(config_path)
+    # run(config_path)
+
+    test_run(config_path)
---
+++ meat/iris/one_hot_test.py
@@ -0,0 +1,174 @@
+from sklearn.preprocessing import OneHotEncoder
+
+from sklearn.preprocessing import LabelEncoder
+
+import numpy as np
+
+
+def test():
+    le = LabelEncoder()
+
+    res = le.fit_transform(['a','b','c'])
+
+    print(res)
+
+
+    enc = OneHotEncoder()
+
+    # enc.fit()
+
+    res = enc.fit_transform([[0],[1],[2]])
+
+    print(res.toarray())
+
+
+class ZxxEncoder():
+    def __init__(self, labels=None):
+        assert labels is not None
+        self.le = LabelEncoder()
+        self.le.fit(labels)
+        res = self.le.transform(labels)
+        self.oh = OneHotEncoder()
+        self.oh.fit(np.reshape(res,(-1,1)))
+        self.tmp_out = self.oh.transform(np.reshape(res,(-1,1)))
+
+    def transform(self, y):
+        tmp = self.le.transform(y)
+        print(tmp)
+        res = self.oh.transform(np.reshape(tmp, (-1,1))).toarray()
+        print(res)
+        return res
+
+    def inverse_transform_v1(self, y):
+
+        decode_columns = np.vectorize(lambda col: self.oh.active_features_[col])
+        print("decode_columns:")
+        print(decode_columns)
+
+
+        print(np.shape(y))
+        print(np.shape(self.tmp_out))
+
+        decoded = decode_columns(y.indices).reshape(-1,np.shape(self.tmp_out)[-1])
+        print("decoded")
+        print(decoded)
+
+        recovered_y = decoded - self.oh.feature_indices_[:-1]
+
+        res = self.le.inverse_transform(recovered_y)
+        print(res)
+        return res
+
+    def inverse_transform(self, y):
+
+        decode_columns = np.vectorize(lambda col: self.oh.active_features_[col])
+        print("decode_columns:")
+        print(decode_columns)
+
+
+        print(np.shape(y))
+        print(np.shape(self.tmp_out))
+
+        decoded = decode_columns(y.indices).reshape(-1,np.shape(self.tmp_out)[-1])
+        print("decoded")
+        print(decoded)
+
+        recovered_y = decoded - self.oh.feature_indices_[:-1]
+
+        res = self.le.inverse_transform(recovered_y)
+        print(res)
+        return res
+    
+def TestZxxEncoder():
+    labels = ['a','b','c']
+
+    candis = ['c','a','a','b']
+
+    ze = ZxxEncoder(labels)
+
+    res = ze.transform(candis)
+
+    print(res)
+
+    candis = ['c'] 
+    res = ze.transform(candis)
+
+    print(res)
+
+    # inverse_candis = [
+    #     [0., 0., 1.],
+    #     [1., 0., 0.],
+    #     [1., 0., 0.],
+    #     [0., 1., 0.]
+    # ]
+
+    # res = ze.inverse_transform(inverse_candis)
+
+    # print(res)
+
+def TestNumpyReshape():
+
+    a = [2,3,4]
+
+    print(np.shape(a))
+
+    print(np.reshape(a, (-1,1)))
+
+
+    a = [2]
+
+    print(np.shape(a))
+
+    print(np.reshape(a, (-1,1)))
+
+
+def printObjectDict(obj):
+    for k,v in obj.__dict__.items():
+        print(k)
+        print(v)
+        print()
+
+def traverseOneHotEncoder():
+    ohe = OneHotEncoder()
+    printObjectDict(ohe)
+    print(ohe.fit_transform([
+        [1],[5],[9]
+    ]).toarray())
+    print(ohe.fit_transform([
+        [1],[5],[9]
+    ]))
+    printObjectDict(ohe)
+
+
+
+    print("\n\n-----------------------\n\n")
+    X = [
+        [1],[5],[9]
+    ]
+    out = ohe.fit_transform(X)
+    printObjectDict(out)
+
+    print(out.sorted_indices())
+
+    decode_columns = np.vectorize(lambda col: ohe.active_features_[col])
+    print("decode_columns:")
+    print(decode_columns)
+
+    decoded = decode_columns(out.indices).reshape(np.shape(X))
+    print("decoded")
+    print(decoded)
+
+    recovered_X = decoded - ohe.feature_indices_[:-1]
+    print("recovered_X")
+    print(recovered_X)
+
+        
+
+if "__main__" == __name__:
+
+    # TestNumpyReshape()
+
+    TestZxxEncoder()
+
+    # traverseOneHotEncoder()
+
