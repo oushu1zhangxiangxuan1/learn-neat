6b104e4ce16c8d5d075b50f54a967a91dd3f29e6
oushu1zhangxiang1 <zhang.xiangxuan@oushu.io>
2020-01-02 19:35:51 +0800

    iris done

--- meat/iris/iris.py
+++ meat/iris/iris.py
@@ -4,6 +4,8 @@ import os
 import neat  
 import visualize 
 import pandas as pd 
+import traceback
+import math
 # import sklearn as 
 
 from sklearn.preprocessing import OneHotEncoder
@@ -12,6 +14,8 @@ from sklearn.preprocessing import LabelEncoder
 
 import numpy as np
 
+DEBUG = False
+
 # from . import init
 
 
@@ -19,7 +23,8 @@ import numpy as np
 
 class ZxxEncoder():
     def __init__(self, labels):
-        print(labels)
+        if DEBUG:
+            print(labels)
         assert labels is not None
         self.le = LabelEncoder()
         self.le.fit(labels)
@@ -30,9 +35,11 @@ class ZxxEncoder():
 
     def transform(self, y):
         tmp = self.le.transform(y)
-        print(tmp)
+        if DEBUG:
+            print(tmp)
         res = self.oh.transform(np.reshape(tmp, (-1,1))).toarray()
-        print(res)
+        if DEBUG:
+            print(res)
         return res
 
     def inverse_transform_v1(self, y):
@@ -131,16 +138,26 @@ def eval_genomes(genomes, config):
             output = net.activate(row.values[:4])
             # 这里需要改用cross entropy when task is classification
             # genome.fitness -= (output[0]-row[4])**2
+            if DEBUG:
+                print(output)
+                print(row.values[4])
 
-            print(output)
-            print(row.values[4])
+            label_encoded = zxxEncoder.transform([row.values[4]])
+            # res = np.reshape(res, (-1))
+            label_encoded = np.ravel(label_encoded).tolist()
+            if DEBUG:
+                print("label: {}".format(row.values[4]))
+                print("after encoding: {}".format(label_encoded))
 
-            res = zxxEncoder.transform([row.values[4]])
-            print("label: {}".format(row.values[4]))
-            print("after encoding: {}".format(res))
+            soft_res = ZxxSoftmax(output)
+            if DEBUG:
+                print("soft_res: {}".format(soft_res))
 
-            if output[0]!= row.values[4]:
-                genome.fitness -= 1
+            loss_cross = ZxxCrossEntropy(soft_res,label_encoded)
+            loss_mse = MultiMSE(soft_res, label_encoded)
+
+            # if output[0]!= row.values[4]:
+            genome.fitness -= loss_cross
 
 
 def run(config_file):
@@ -158,7 +175,7 @@ def run(config_file):
     p.add_reporter(neat.Checkpointer(5))
 
     # Run for up to 300 generations.
-    winner = p.run(eval_genomes, 30)
+    winner = p.run(eval_genomes, GENS)
 
     #Display the winning genome.
     print('\n Best genome:\n{!s}'.format(winner))
@@ -166,18 +183,31 @@ def run(config_file):
     # Show output of the most fit genome against training data.
     print('\nOutput:')
     winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
+    predictNum = iris_data.shape[0]
+    predictRight = 0.0
     for i,row in iris_data.iterrows():
         output = winner_net.activate(row.values[:4])
-        print('input {!r}, expected output {!r}, got {!r}'.format(
-            row.values[:4], row.values[4], output
-        ))
+        index = output.index(max(output))
+        expect_out = np.ravel(zxxEncoder.transform([row.values[4]]))
+        expect_index = expect_out.tolist().index(max(expect_out))
+
+        if index == expect_index:
+            predictRight += 1
+
+        # print('expected index {!r}, got {!r}     expected output {!r}, got {!r}   '.format(
+        #     index, expect_index, expect_out, output
+        # ))
+    accuracy = predictRight/predictNum
+    print("\nAccuracy: {}".format(accuracy), flush=True)
 
     # How does the node_names work?
-    node_names = {-1:'A', -2:'B', -3:'C', -4:'D', 0:'label'}
-    visualize.draw_net(config, winner, True, node_names=node_names)
+    node_names = {-1:'A', -2:'B', -3:'C', -4:'D', 0:'label_0', 1:'label_1', 2:'label_2'}
+    visualize.draw_net(config, winner, False, node_names=node_names)
 
-    p = neat.Checkpointer.restore_checkpoint('neat-chepoint-4')
-    p.run(eval_genomes, 10)
+    # p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-4')
+    # p.run(eval_genomes, 1)
+
+    return accuracy
 
 
 def test_run(config_file):
@@ -217,6 +247,43 @@ def test_run(config_file):
     # p.run(eval_genomes, 10)
 
 
+def ZxxSoftmax(src):
+    assert isinstance(src, list)
+    assert len(src)>1
+    denominator = sum([math.e ** i for i in src])
+    return [(math.e ** i)/denominator for i in src]
+
+def ZxxCrossEntropy(src, target):
+    if DEBUG:
+        print("src: {}".format(src))
+        print("target: {}".format(target))
+        print("src sum: {}".format(sum(src)))
+    assert isinstance(src, list)
+    assert len(src)==len(target)
+    assert isinstance(target, list)
+    assert len(target)>1
+    # assert int(sum(src))==1.0
+    # assert int(sum(target)) == 1.0
+    # assert target.index(1)>0
+    try:
+        i = target.index(1)
+        loss = -math.log(src[i])
+        return loss
+    except Exception as e:
+        traceback.format_exc(e)
+        raise e
+
+def MultiMSE(src, target):
+    assert isinstance(src, list)
+    assert len(src)==len(target)
+    assert isinstance(target, list)
+    assert len(target)>1
+    # assert sum(src)==1
+    # assert sum(target) == 1
+
+    return sum([(src[i]-target[i])**2 for i in range(len(src))])
+
+
 
 
 
@@ -226,7 +293,7 @@ if __name__ == '__main__':
 
     labels = getIrisLabels(iris_data)
 
-    print("Iris labels: {}".format(labels))
+    # print("Iris labels: {}".format(labels))
 
     zxxEncoder = ZxxEncoder(labels)
 
@@ -237,8 +304,13 @@ if __name__ == '__main__':
     # current working directory.
     local_dir = os.path.dirname(__file__)
     config_path = os.path.join(local_dir, 'config_feedforward')
-    # run(config_path)
+    GENS=100
+    while True:
+        acc = run(config_path)
+        if acc > 0.85 or GENS > 500:
+            break
+        GENS += 10
 
-    test_run(config_path)
+    # test_run(config_path)
 
     # getIrisLabels()
