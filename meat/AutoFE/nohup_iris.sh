06c24bb40da9051b22905186ec9d9714e8e062d4
oushu1zhangxiang1 <zhang.xiangxuan@oushu.io>
2020-01-20 15:29:27 +0800

    file name template bug fix

--- meat/AutoFE/nohup_iris.sh
+++ meat/AutoFE/nohup_iris.sh
@@ -1,5 +1,5 @@
 # echo "result will be write to $1"
 # nohup python -u ./iris.py --train_data=../../testdata/iris_feature_selection/iris_train.csv --test_data=../../testdata/iris_feature_selection/iris_test.csv > $1 2>&1 & 
 
-nohup python -u ./iris.py --train_data=../../testdata/iris_feature_selection/ones/iris_train.csv --test_data=../../testdata/iris_feature_selection/ones/iris_test.csv > ones.out 2>&1 & 
-nohup python -u ./iris.py --train_data=../../testdata/iris_feature_selection/random/iris_train.csv --test_data=../../testdata/iris_feature_selection/random/iris_test.csv > random.out 2>&1 & 
+nohup python -u ./iris.py --train_data=../../testdata/iris_feature_selection/ones/iris_train.csv --test_data=../../testdata/iris_feature_selection/ones/iris_test.csv --name=ones > ones.out 2>&1 & 
+nohup python -u ./iris.py --train_data=../../testdata/iris_feature_selection/random/iris_train.csv --test_data=../../testdata/iris_feature_selection/random/iris_test.csv --name=random > random.out 2>&1 & 
